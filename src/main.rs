391ca16f187e60efeaf7e547a4d6095adc092691
Tommaso Ricci <zorgatone@gmail.com>
2018-11-18 18:47:27 +0100

    Added Error handling

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,10 @@ fn main() {
     io::stdin().read_line(&mut guess)
       .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse()
-      .expect("Please type a number!");
+    let guess: u32 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => continue,
+    };
 
     println!("You guessed: {}", guess);
 
@@ -30,7 +32,7 @@ fn main() {
       Ordering::Equal => {
         println!("You win!");
         break;
-      }
+      },
     }
   }
 }
